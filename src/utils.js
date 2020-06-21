3965e29dab959cdf8197d89390117cfce601ee44
Rajesh Shenoy <rajeshtshenoy@gmail.com>
2020-06-21 21:44:25 +0100

    baseurl ulback.heroku

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = "http://localhost:5000/";;
+export const baseUrl = "https://ulback.herokuapp.com/";
 export const sortByPrice = async () => {
   const res = await axios.get(baseUrl);
   return res.data.data.sort((a, b) => a.price.value - b.price.value);
